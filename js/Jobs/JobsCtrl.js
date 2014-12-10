79dbef3401292d63cdd9edcbe250b4482a6e731e
calbu <cipri_albu@yahoo.com>
2014-12-10 09:58:43 +0200

    jobs controller
    jobs controller schema

---
+++ js/Jobs/JobsCtrl.js
@@ -0,0 +1,11 @@
+﻿(function () {
+
+    var app = angular.module("tutorialWebApp");
+        
+    var JobsCtrl = function ($scope, ProjectCouch) {
+        console.log("test Jobs reporting for duty.");
+    };
+
+    app.controller("JobsCtrl", ['$scope', 'ProjectCouch', JobsCtrl]);
+
+}());
