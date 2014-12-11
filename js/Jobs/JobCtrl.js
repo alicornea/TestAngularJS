9f3c45c83b5c70c2c9911a67daf2c450ce4366c0
calbu <cipri_albu@yahoo.com>
2014-12-11 18:04:42 +0200

    fsfds
    sdfsd

--- js/Jobs/JobCtrl.js
+++ js/Jobs/JobCtrl.js
@@ -32,6 +32,9 @@
             var dd = today.getDate();
             var mm = today.getMonth() + 1; //January is 0!
             var yyyy = today.getFullYear();
+            var hh = today.getHours();
+            var min = today.getMinutes();
+            var sec = today.getSeconds();
 
             if (dd < 10) {
                 dd = '0' + dd
@@ -41,7 +44,18 @@
                 mm = '0' + mm
             }
 
-            return today = mm + '/' + dd + '/' + yyyy;
+            hh = addZeroIfNecessary(hh);
+            min = addZeroIfNecessary(min);
+            sec = addZeroIfNecessary(sec);
+
+            return today = mm + '/' + dd + '/' + yyyy + ' ' + hh + ":" + min + ":" + sec;
+        };
+
+        function addZeroIfNecessary(field) {
+            if (!isNaN(field) && field < 10) {
+                return field = "0" + field;
+            }
+            return field;
         };
     };
 
--- js/Jobs/NewJobCtrl.js
+++ js/Jobs/NewJobCtrl.js
@@ -19,6 +19,9 @@
             var dd = today.getDate();
             var mm = today.getMonth() + 1; //January is 0!
             var yyyy = today.getFullYear();
+            var hh = today.getHours();
+            var min = today.getMinutes();
+            var sec = today.getSeconds();
 
             if (dd < 10) {
                 dd = '0' + dd
@@ -28,7 +31,18 @@
                 mm = '0' + mm
             }
 
-            return today = mm + '/' + dd + '/' + yyyy;
+            hh = addZeroIfNecessary(hh);
+            min = addZeroIfNecessary(min);
+            sec = addZeroIfNecessary(sec);
+
+            return today = mm + '/' + dd + '/' + yyyy + ' ' + hh + ":" + min + ":" + sec;
+        };
+
+        function addZeroIfNecessary(field) {
+            if(!isNaN(field) && field < 10)
+            {
+                return field = "0" + field;
+            }
         };
     };
 
