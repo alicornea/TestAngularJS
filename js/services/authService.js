8af1af3c7f738cf7f841cf9f5cdb2314d7a2de1b
Andrei Murgu <alexandru.murgu@yahoo.com>
2014-12-12 13:42:43 +0200

    Authentication
    [+] Login partial
    [+] Logout
    [+] Save user info in session
    [+] Login form validation

--- js/services/authService.js
+++ js/services/authService.js
@@ -1,44 +1,53 @@
 (function() {
 
-  var AuthService = function($resource) {
+  var AuthService = function(ProjectCouch, $window) {
 
     var login = function(credentials) {
-      var promise = $resource(':protocol//:server/:db/:q/:r/:s/:t', {
-        protocol: 'http:',
-        server: 'alicornea.iriscouch.com',
-        db: 'test_angular',
-	q: '_design',
+      var promise = ProjectCouch.get({
+        q: '_design',
         r: 'users',
         s: '_view',
         t: 'getAll',
         include_docs: 'true',
-        limit: 10
-      }, {
-        get: {
-          method: 'GET'
-        }
-      }).get();
-
+        limit: 10,
+        key: '"' + credentials.username + '"'
+      });
 
       return promise.$promise
         .then(function(result) {
-          return result;
-        }, function(reason) {
-          //reason;
+          var userInfo = {};
+          if (result.rows.length > 0) {
+            angular.forEach(result.rows, function(user) {
+              if (user.value.password == credentials.password) {
+                var userInfo = {
+                  accessToken: user.id,
+                  username: credentials.username
+                };
+                $window.sessionStorage.userInfo = JSON.stringify(userInfo);
+              }
+            });
+          }
+
+          return userInfo;
         });
     };
 
     var isAuthenticated = function() {
-      //return !!Session.userId;
+      return $window.sessionStorage.getItem("userInfo") != null && !!JSON.parse($window.sessionStorage.getItem("userInfo")).accessToken;
+    };
+
+    var logout = function() {
+      $window.sessionStorage.removeItem("userInfo");
     };
 
     return {
       login: login,
       isAuthenticated: isAuthenticated,
+      logout: logout
     };
   };
 
   var app = angular.module("tutorialWebApp");
-  app.factory("AuthService", ['$resource', AuthService]);
+  app.factory("AuthService", ['ProjectCouch', '$window', AuthService]);
 
 }());
