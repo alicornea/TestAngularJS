945ef892267c5502f9644fe50144944edb152b06
Andrei Murgu <alexandru.murgu@yahoo.com>
2014-12-15 17:24:19 +0200

    Redirect to index if not authorized

--- js/filters/authorize.js
+++ js/filters/authorize.js
@@ -2,12 +2,8 @@
     angular.module("mrgApp").
     run(function($location, $rootScope, $route, USER_ROLES, AuthService) {
         $rootScope.$on('$routeChangeStart', function(event, next, current) {
-            if (next.$$route.authorizedRoles !== undefined && !AuthService.isAuthorized(next.$$route.authorizedRoles)) {
-                if(current.$$route.originalPath !== undefined){
-			$location.path(current.$$route.originalPath);
-		} else {
-			$location.path("/");
-		}		
+            if (next.$$route.authorizedRoles !== undefined && !AuthService.isAuthorized(next.$$route.authorizedRoles)) {                
+		$location.path("/");				
             }
         });
     });
