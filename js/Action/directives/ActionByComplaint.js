bf70199fae0c071a989cb24b1ebf2f9db12e0cea
monica-moga <monica_moga2002@yahoo.com>
2014-12-17 15:58:45 +0200

    activities- add service and directive

---
+++ js/Action/directives/ActionByComplaint.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    var app = angular.module("mrgApp");
+    app.directive("activitiesComplaint", ['ActionService', activitiesComplaint]);
+
+    function activitiesComplaint(ActionService) {
+
+        function myLink(scope, elements, attributes) {
+            console.log("complaint id = " + scope.complaintid);
+
+            var promise = (typeof scope.complaintid != "undefined")
+                ? ActionService.GetActionsByComplaintId(scope.complaintid)
+                : ActionService.GetAllActions();
+
+            var ProcessData = function (data) {
+                scope.actions = data;
+            };
+
+            var ProcessError = function (reason) {
+                alert(reason);
+            };
+            promise.$promise.then(ProcessData, ProcessError);
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                complaintid: "="
+            },
+            templateUrl: 'partials/Action/directives/action.html',
+            link: myLink
+        }
+    };
+
+
+
+}());
---
+++ js/Common/actionService.js
@@ -0,0 +1,40 @@
+﻿(function () {
+
+    var ActionService = function (ProjectCouch) {
+      
+        function GetActions()
+        {
+            console.log("getallActions");
+            var promise = ProjectCouch.get({
+                q: '_design',
+                r: '_actions',
+                s: '_view',
+                t: 'getAll',
+                include_docs: 'true',
+                limit: 10
+            });
+            return promise;
+        }
+
+        function GetActionsBasedOnComplaintId(complaintId) {
+            console.log("getactionsbased on compliant");
+            var promise = ProjectCouch.get({
+                q: '_design',
+                r: '_actions',
+                s: '_view',
+                t: 'byCompliantId',
+                key: "\"" + complaintId + "\"",
+                include_docs: 'true',
+                limit: 10
+            });
+            return promise;
+        }
+
+        return { GetAllActions: GetActions , GetActionsByComplaintId: GetActionsBasedOnComplaintId };
+    };
+
+    var app = angular.module("mrgApp");
+    app.factory("ActionService", ["ProjectCouch", ActionService]);
+
+
+}());
