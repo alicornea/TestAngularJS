124d3a69c5c0d03f26311a27988197102369b47c
monica_moga <monica_moga2002@yahoo.com>
2015-01-06 13:28:29 +0000

    add param to activities by complaint directive

--- js/Action/directives/ActionByComplaint.js
+++ js/Action/directives/ActionByComplaint.js
@@ -5,11 +5,9 @@
     function activitiesComplaint(ActionService, $routeParams) {
 
         function myLink(scope, elements, attributes) {
-           var complaintid = $routeParams.id;
-            console.log("complaint id2 = " + complaintid );
-
-            var promise = (typeof complaintid != "undefined")
-                ? ActionService.GetActionsByComplaintId(complaintid)
+          
+            var promise = (typeof scope.complaintid != "undefined")
+                ? ActionService.GetActionsByComplaintId(scope.complaintid)
                 : ActionService.GetAllActions();
 
             var ProcessData = function (data) {
@@ -24,7 +22,9 @@
 
         return {
             restrict: 'E',
-
+            scope: {
+                complaintid: '='
+            },
             templateUrl: 'partials/action/directives/action.html',
             link: myLink
         }
--- js/complaints/ComplaintCtrl.js
+++ js/complaints/ComplaintCtrl.js
@@ -1,7 +1,10 @@
 (function() {
     angular.module("mrgApp").controller('ComplaintCtrl', function($scope, $routeParams, ComplaintsService, DataService) {
+        $scope.currentComplaintId =  $routeParams.id; 
+        
         ComplaintsService.getComplaint($routeParams.id).then(function(data) {
             $scope.complaint = data.rows[0];
+           
         }, function(reason) {
             alert(reason);
         });
