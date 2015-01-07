33514e0de6e85a49dc577c85a9308ec8e341b1b0
calbu <albu.cipri@gmail.com>
2015-01-07 12:31:09 +0000

    add controller to index

--- js/Jobs/AttachmentsCtrl.js
+++ js/Jobs/AttachmentsCtrl.js
@@ -1,10 +1,9 @@
 (function() {
     var app = angular.module("mrgApp");
-    alert("ce faci mha");
-    
-    var AttachmentsCtrl = function ($scope, $location, ProjectCouch) {
+
+    var AttachmentsCtrl = function($scope, $location, ProjectCouch) {
         console.log("Attachements reporting for duty.");
     };
 
-    app.controller("AttachmentsCtrl", ['$scope', '$location', 'ProjectCouch', AttachmentsCtrl]);    
+    app.controller("AttachmentsCtrl", ['$scope', '$location', 'ProjectCouch', AttachmentsCtrl]);
 }());
