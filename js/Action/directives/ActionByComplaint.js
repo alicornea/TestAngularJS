4bcfcd66bfbd3c627170d545885f7449d482ab69
andreisantaaccenture <andrei.santa@accenture.com>
2015-01-15 14:13:46 +0000

    change complaint actions

--- js/Action/directives/ActionByComplaint.js
+++ js/Action/directives/ActionByComplaint.js
@@ -1,35 +1,24 @@
-﻿(function () {
-    var app = angular.module("mrgApp");
-    app.directive("activitiesComplaint", ['ActionService','$routeParams', activitiesComplaint]);
-
-    function activitiesComplaint(ActionService, $routeParams) {
-
-        function myLink(scope, elements, attributes) {
-          
-            var promise = (typeof scope.complaintid != "undefined")
-                ? ActionService.GetActionsByComplaintId(scope.complaintid)
-                : ActionService.GetAllActions();
-
-            var ProcessData = function (data) {
-                scope.actions = data;
-            };
-
-            var ProcessError = function (reason) {
-                console.log(reason);
-            };
-            promise.$promise.then(ProcessData, ProcessError);
-        }
+(function() {
+    angular.module("mrgApp").directive("complaintActions", ['ActionService', '$routeParams', function(ActionService, $routeParams) {
 
         return {
             restrict: 'E',
             scope: {
                 complaintid: '='
             },
-            templateUrl: 'partials/action/directives/action.html',
-            link: myLink
+            templateUrl: 'partials/action/directives/Actions.html',
+            link: function myLink(scope, elements, attributes) {
+                var promise = (typeof scope.complaintid != "undefined") ? ActionService.GetActionsByComplaintId(scope.complaintid) : ActionService.GetAllActions();
+
+                var ProcessData = function(data) {
+                    scope.actions = data;
+                };
+
+                var ProcessError = function(reason) {
+                    console.log(reason);
+                };
+                promise.$promise.then(ProcessData, ProcessError);
+            }
         }
-    };
-
-
-
+    }]);
 }());
